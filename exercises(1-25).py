32190c3b0ee6326d28faabf65ddd44a526af15bd
FaizanMuhammadSE <jutt41697@gmail.com>
2024-09-09 15:52:04 +0500

    Add exercises 24 and 25 with solutions

--- exercises(1-25).py
+++ exercises(1-25).py
@@ -222,3 +222,23 @@ pprint(d)
 # Expected output: 13  
 d = dict(a = list(range(1, 11)), b = list(range(11, 21)), c = list(range(21, 31)))
 print(d["b"][2])
+
+
+################################ Exercise#24 (Iterate Dictionary)
+# Question: Please complete the script so that it prints out the expected output.
+
+# d = dict(a = list(range(1, 11)), b = list(range(11, 21)), c = list(range(21, 31)))
+# Expected output: 
+
+# b has value [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# c has value [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+# a has value [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+d = dict(a = list(range(1, 11)), b = list(range(11, 21)), c = list(range(21, 31)))
+for x,y in d.items():
+    print(f'{x} has value {y}')
+
+
+################################ Exercise#25 (print a to z)
+for i in range(97,123):
+    print(chr(i))
